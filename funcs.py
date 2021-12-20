508157e76d42d38abaedd73c8644804a8dcc47b8
Laura Jacoby <ljacoby@uw.edu>
2021-12-19 23:49:02 -0800

    sentence and paragraph funcs

--- funcs.py
+++ funcs.py
@@ -22,3 +22,19 @@ def year_counter(current, earliest):
     years = [str(year)+"-"+str(year) for year in range(current, earliest, -1)
             ]
     return years
+
+def sentence(phrase1, phrase2):
+    sentence = '{} and {} make me happy'.format(phrase1,phrase2)
+    return sentence
+
+def paragraph(sentence):
+    pairs = [('one', 'two'),('cows', 'blue'),('three', 'four')]
+
+    paragraph = []
+    for p in pairs:
+        sent = sentence(p[0], p[1])
+        paragraph.append(sent)
+
+    final_p = '.'.join(paragraph)
+
+    return(final_p)
